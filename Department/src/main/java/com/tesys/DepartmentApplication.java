fb40c5e632c722145471d49726c1f04899533fb3
danielmicheli <danielmicheli@outlook.it>
2024-05-31 09:34:28 +0200

    add eureka

--- Department/src/main/java/com/tesys/DepartmentApplication.java
+++ Department/src/main/java/com/tesys/DepartmentApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.tesys"})
+
 public class DepartmentApplication {
 
 	public static void main(String[] args) {
